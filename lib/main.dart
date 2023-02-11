be2c3d4dbb99b0e8184dc9133668a54372b8f237
Denize Ignacio <denizepatricignacio@gmail.com>
2023-02-10 19:49:59 -0800

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              width: 100.0,
+              color: Colors.red,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(width: 100, height: 100, color: Colors.yellow),
+                Container(width: 100, height: 100, color: Colors.green),
+              ],
+            ),
+            Container(
+              width: 100.0,
+              color: Colors.blue,
+            ),
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
